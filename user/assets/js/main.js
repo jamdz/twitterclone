175c7908ddd25eed3e9160e5ded68c13a02503eb
jamdz <jamesiucky926@gmail.com>
2020-09-16 11:56:06 -1200

    my inital commit

---
+++ user/assets/js/main.js
@@ -0,0 +1,5 @@
+let cenralNav = document.querySelector(".central-nav-img");
+let leftNav = document.querySelector(".left-side-bar");
+cenralNav.addEventListener("click", function() {
+    leftNav.classList.toggle = "show";
+});
